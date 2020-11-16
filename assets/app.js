89c24428f915c696efce20368228d96708e71f2d
Yunus Emre Mert <yunusemremert@yandex.com>
2020-11-16 23:19:16 +0300

    Bootstrap

--- assets/app.js
+++ assets/app.js
@@ -11,4 +11,4 @@ import './styles/app.css';
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
-console.log('Hello Webpack Encore! Edit me in assets/app.js');
+require("bootstrap/dist/css/bootstrap.css");
